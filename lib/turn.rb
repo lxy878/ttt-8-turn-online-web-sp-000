f6a8196adf9f9f493956f3ab296e80a65904be8c
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 06:11:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,9 @@ end
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
@@ -16,7 +18,7 @@ def valid_move?(board, index)
   if index < 0
     return false
   end
-  
+
   if board[index] != "X" or board[index] != "O"
     return true
   end
