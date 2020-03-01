e9a95e49d617ae41789f131aacc63091290ff7a5
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 06:46:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,10 @@ def turn(board)
   puts "Please enter 1-9:"
   index = gets
   index = input_to_index(index)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  end
 end
 
 def display_board(board)
