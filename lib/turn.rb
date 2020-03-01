74cfea5fecefafce832a48e2ce15b47739250aed
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 06:01:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,10 +2,15 @@ def turn
 
 end
 
-def display_board
-
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  return input.to_i - 1
+end
 def valid_move?
 
 end
